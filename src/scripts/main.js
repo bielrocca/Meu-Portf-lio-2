54c7daf01d3e451c61e64b2225cb8daf07124bcc
Gabriel Lucas <bielfefi@outlook.com>
2024-07-24 13:36:11 -0300

    Comentario Inicial

---
+++ src/scripts/main.js
@@ -0,0 +1,55 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const buttons = document.querySelectorAll('[data-tab-button]');
+
+    const heroSection = document.querySelector('.wrapper')
+    const alturaWrapper = heroSection.clientHeight
+
+    window.addEventListener('scroll', function() {
+        const posicaoAtual = window.scrollY
+
+        if(posicaoAtual < alturaWrapper) {
+            ocultaElementosDoHeader()
+        } else {
+            exibeElementosDoHeader()
+        }
+    })
+
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', function(botao) {
+            const abaAlvo = botao.target.dataset.tabButton;
+            const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`)
+            hideTabs();
+            aba.classList.add('shows__list--is-active')
+            removeActiveButtons();
+            botao.target.classList.add('shows__tabs__button--is-active')
+        })
+    }
+})
+
+function ocultaElementosDoHeader() {
+    const header = document.querySelector('header')
+    header.classList.add('header--is-hidden')
+}
+
+function exibeElementosDoHeader() {
+    const header = document.querySelector('header')
+    header.classList.remove('header--is-hidden')
+}
+
+function removeActiveButtons() {
+    const buttons = document.querySelectorAll('[data-tab-button]');
+
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove('shows__tabs__button--is-active')
+    }
+}
+
+function hideTabs() {
+    const tabsContainer = document.querySelectorAll('[data-tab-id]');
+
+    for (let i = 0; i < tabsContainer.length; i++) {
+        tabsContainer[i].classList.remove('shows__list--is-active')
+    }
+}
+
+AOS.init();
